d6df31ea2e514eb28bc94f1ee28af73bfd4f12b7
nick-occ <nicholaso1980@gmail.com>
2017-02-05 20:56:49 -0500

    code

--- src/OpenGameList/Startup.cs
+++ src/OpenGameList/Startup.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using OpenGameList.Data;
+
+using Nelibur.ObjectMapper;
+using OpenGameList.Data.Items;
+using OpenGameListWebApp.ViewModels;
 
 namespace OpenGameList
 {
@@ -29,15 +36,68 @@ namespace OpenGameList
         {
             // Add framework services.
             services.AddMvc();
+
+            //Add EntityFramework's Identity support.
+            services.AddEntityFramework();
+
+            //Add ApplicationDbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+
+            //Add ApplicationDbContext's DbSeeder
+            services.AddSingleton<DbSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            DbSeeder dbSeeder)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // Configure a rewrite rule to auto-lookup for standard default files such as index.html. 
+            app.UseDefaultFiles();
+
+            // Serve static files (html, css, js, images & more). See also the following URL: 
+            // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference. 
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+
+                OnPrepareResponse = (context) =>
+
+                {
+
+                    // Disable caching for all static files.
+
+                    context.Context.Response.Headers["Cache-Control"] =
+                    Configuration["StaticFiles:Headers:Cache-Control"];
+
+                    context.Context.Response.Headers["Pragma"] =
+                    Configuration["StaticFiles:Headers:Pragma"];
+
+                    context.Context.Response.Headers["Expires"] =
+                    Configuration["StaticFiles:Headers:Expires"];
+
+                }
+            });
+
+
             app.UseMvc();
+
+            // TinyMapper binding configuration
+            TinyMapper.Bind<Item, ItemViewModel>();
+
+            // Seed the Database (if needed)
+            try
+            {
+                dbSeeder.SeedAsync().Wait();
+            }
+            catch (AggregateException e)
+            {
+                throw new Exception(e.ToString());
+            }
         }
     }
 }
